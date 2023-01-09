d434063ddadac9408b9dd78602742a95f1fdbf66
erkaevrus <erk_08@mail.ru>
2023-01-09 10:25:42 +0500

    complete

--- src/index.js
+++ src/index.js
@@ -38,8 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arrDigitWord = expr.split('**********')
+    const arrDigitAlpha = []
+    let line = []
+    let letter = ''
+    let result = ''
+
+    for (let item of arrDigitWord) {
+        for (let i = 0; i < item.length; i += 10) {
+        line.push(item.slice(i, i+10))
+        }
+          arrDigitAlpha.push(line)
+          line = []
+    }
+
+    for (let item of arrDigitAlpha) {
+        for (let str of item) {
+            for (let i = 0; i < str.length; i +=2) {
+                if (str.slice(i, i + 2) === '10') {
+                    letter += '.'
+                }
+                if (str.slice(i, i + 2) === '11') {
+                    letter += '-'
+                }
+            }
+            result += MORSE_TABLE[letter]
+            letter = ''
+        }
+        result += ' '
+    }
+    return result.trim()
 }
+
 
 module.exports = {
     decode
